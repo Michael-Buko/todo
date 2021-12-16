b8927414f1b55eed1e09a8eb910c1855e4009897
Michael Buko <5610280@gmail.com>
2021-12-16 19:48:39 +0300

    feat(todo): added todo list

--- script.js
+++ script.js
@@ -5,6 +5,17 @@ function app() {
 
 app()
 
+// COMPONENTS
+function createTodoList(todos) {
+    const list = createElement('div', 'd-flex flex-column gap-1', '')
+    list.id = 'list'
+    todos.forEach(todo => {
+        const todoItem = createTodoItem(todo)
+        list.append(todoItem)
+    })
+    return list
+}
+
 // UTILS
 function createElement(tag, className, text = '') {
     const element = document.createElement(tag)
